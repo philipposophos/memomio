5a2cd943639992ce4b0c28e09e69fa2f536bf610
Philipp Staehr <philippstaehr@gmail.com>
2023-03-21 14:49:10 +0100

    added create + destroy actions

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -6,8 +6,27 @@ class TagsController < ApplicationController
   end
 
   def create
+    @memory = Memory.find(params[:memory_id])
+    @tag = Tag.new(tag_params)
+    @tag.memory = @memory
+
+    if @tag.save
+      redirect_to memory_path(@memory)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @tag = Tag.find(params[:id])
+    @memory = @tag.memory
+    @tag.destroy
+    redirect_to memory_path(@memory)
+  end
+
+  private
+
+  def tag_params
+    params.require(:tag).permit(:emotional_tag_id)
   end
 end
