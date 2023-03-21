d58701ad854a57203af4e4116e281b867d60b4cd
Philipp Staehr <philippstaehr@gmail.com>
2023-03-21 13:08:26 +0100

    added new-action + new-view for tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,2 +1,13 @@
 class TagsController < ApplicationController
+  def new
+    @memory = Memory.find(params[:memory_id])
+    @tag = Tag.new
+    @emotional_tags = EmotionalTag.all
+  end
+
+  def create
+  end
+
+  def destroy
+  end
 end
