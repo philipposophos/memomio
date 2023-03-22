ba6144257b768eecd072606897db9665851b70ef
Paulyn Ompico <ompicopaulyn@gmail.com>
2023-03-22 14:47:20 +0100

    Add pundit policies for Memory and Note

--- app/controllers/memories_controller.rb
+++ app/controllers/memories_controller.rb
@@ -1,24 +1,28 @@
 class MemoriesController < ApplicationController
   def index
     if params[:query].present?
-      @memories = Memory.where(title: params[:query])
+      @memories = policy_scope(Memory).where(title: params[:query])
     else
-      @memories = Memory.all
+      @memories = policy_scope(Memory)
     end
   end
 
   def new
     @memory = Memory.new
+    authorize @memory
   end
 
   def show
     @memory = Memory.find(params[:id])
     @note = Note.new
+    authorize @memory
+    authorize @note
   end
 
   def create
     @memory = Memory.new(memory_params)
     @memory.user = current_user
+    authorize @memory
 
     if @memory.save
       redirect_to memory_path(@memory)
@@ -29,18 +33,20 @@ class MemoriesController < ApplicationController
 
   def edit
     @memory = Memory.find(params[:id])
+    authorize @memory
   end
 
   def update
     @memory = Memory.find(params[:id])
     @memory.update(memory_params)
+    authorize @memory
     redirect_to memory_path(@memory)
   end
 
   def destroy
     @memory = Memory.find(params[:id])
     @memory.destroy
-
+    authorize @memory
     redirect_to memories_path, status: :see_other
   end
 
