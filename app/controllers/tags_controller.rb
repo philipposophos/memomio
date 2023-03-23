33d1c25a92d2c45a0b58a50d613653ea5aa33443
Philipp Staehr <philippstaehr@gmail.com>
2023-03-23 17:14:17 +0100

    Fixed authorization problem with tags and emotional_tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,12 +3,17 @@ class TagsController < ApplicationController
     @memory = Memory.find(params[:memory_id])
     @tag = Tag.new
     @emotional_tags = EmotionalTag.all
+    authorize @memory
+    authorize @tag
+    authorize @emotional_tags
   end
 
   def create
     @memory = Memory.find(params[:memory_id])
     @tag = Tag.new(tag_params)
     @tag.memory = @memory
+    authorize @memory
+    authorize @tag
 
     if @tag.save
       redirect_to memory_path(@memory)
@@ -20,8 +25,11 @@ class TagsController < ApplicationController
   def destroy
     @tag = Tag.find(params[:id])
     @memory = @tag.memory
+    authorize @tag
+    authorize @memory
     @tag.destroy
     redirect_to memory_path(@memory)
+
   end
 
   private
---
+++ app/policies/tag_policy.rb
@@ -0,0 +1,28 @@
+class TagPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    # def resolve
+    #   scope.all
+    # end
+  end
+
+  def new?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  def update?
+    record.memory.user == user
+  end
+
+  def destroy?
+    record.memory.user == user
+  end
+end
